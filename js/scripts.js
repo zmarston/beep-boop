fd72ecbbccafaffcfcc867543a11e449514c7b20
Zachary Marston <zmarston@live.com>
2017-10-27 09:44:52 -0700

    add js that changes 0,1,3 into words and sentences.

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+var beepBoopNumber = [];
+var translate = function(input) {
+  for (var i = 0; i < input.length; i++) {
+    // debugger;
+    if (input[i] === "0") {
+      beepBoopNumber += "Beep";
+    } else if (input[i] === "1") {
+      beepBoopNumber += "Boop";
+    } else if (input[i] === "3") {
+      beepBoopNumber += "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else {
+      beepBoopNumber += input[i];
+    }
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function(){
+  $("#beep-boop-form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#number-input").val().split("");
+    var beepBoop = translate(userInput);
+
+     alert(beepBoopNumber);
+  });
+
+});
